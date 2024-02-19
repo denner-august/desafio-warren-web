25bfe2d9fb491530aca85cf2b3901e3683b3323c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-19 16:37:25 -0300

    refactor(modal): integrando modal a tabela de registros
    adicionando modal ao component e integrando assim quando apertar em um registro o modal será aberto
    com a informações do registro selecionado

--- desafio-warren-web/src/app/components/main/valores/valores.tsx
+++ desafio-warren-web/src/app/components/main/valores/valores.tsx
@@ -1,11 +1,21 @@
-import { registroProps, ContextProps } from "@/app/context/registerContext";
+import { ContextProps } from "@/app/types/projectTypes";
 import Style from "./valores.module.scss";
+import { Modal } from "../../modal/modal";
+import { useState } from "react";
 
 export function Valores({ RegisterSearch }: Partial<ContextProps>) {
+  const [modal, setModal] = useState(false);
+  const [idRegister, setIdRegister] = useState("");
+
+  function ModalController(id: string) {
+    setIdRegister(id);
+    setModal(true);
+  }
+
   const RenderRegistros = () => {
     return RegisterSearch?.map(({ id, title, description, amount, status }) => {
       return (
-        <tr key={id} className={Style.Registros}>
+        <tr key={id} onClick={() => ModalController(id)}>
           <td>{title}</td>
           <td>{description}</td>
           <td>{status}</td>
@@ -36,6 +46,8 @@ export function Valores({ RegisterSearch }: Partial<ContextProps>) {
           <RenderRegistros />
         </tbody>
       </table>
+
+      <Modal isOpen={modal} setOpen={setModal} id={idRegister} />
     </div>
   );
 }
