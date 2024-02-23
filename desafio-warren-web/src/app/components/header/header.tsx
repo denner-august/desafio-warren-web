a2993e20191c88287b75c5ca6834406a3e6ec037
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-23 19:23:38 -0300

    feat(logo): adiciona logo ao projeto

--- desafio-warren-web/src/app/components/header/header.tsx
+++ desafio-warren-web/src/app/components/header/header.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import styles from "./header.module.scss";
+import logo from "@/../public/icons/budget.png";
 
 export function Header() {
   return (
     <header className={styles.Container}>
-      <h1>logo</h1>
+      <Image
+        src={logo}
+        width={75}
+        height={75}
+        alt="Picture of the author"
+        blurDataURL={"@/../public/icons/budget.png"}
+      />
     </header>
   );
 }
