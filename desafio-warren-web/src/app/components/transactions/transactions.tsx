964bf26e03800888c9bf17991ec20d3d9d43fb14
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-23 19:22:41 -0300

    feat(transactions): agora as transactions tem component proprio

--- desafio-warren-web/src/app/components/transactions/transactions.tsx
+++ desafio-warren-web/src/app/components/transactions/transactions.tsx
@@ -1,3 +1,26 @@
-export function Transactions() {
-  return <h1>Transactions</h1>;
+import styles from "./transactions.module.scss";
+
+interface TransactionsProps {
+  title: string;
+  from?: string;
+  amount?: number;
+}
+
+export function Transactions({ title, from, amount = 0 }: TransactionsProps) {
+  return (
+    <section className={styles.Container}>
+      <article>
+        <p>{title}</p>
+        <ul>
+          <li>{from}</li>
+          <li>
+            {amount.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </li>
+        </ul>
+      </article>
+    </section>
+  );
 }
