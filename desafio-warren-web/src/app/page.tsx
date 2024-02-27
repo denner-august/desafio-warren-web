f47e75a28f29015d5d7b652d4bde6e187efc9ba8
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-27 18:55:12 -0300

    Update page.tsx

--- desafio-warren-web/src/app/page.tsx
+++ desafio-warren-web/src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import logo from "@/../public/icons/budget.png";
+
 import { Header } from "./components/header/header";
 import { Main } from "./components/main/main";
 import { RegisterContextProvider } from "./context/registerContext";
@@ -5,7 +8,17 @@ import { RegisterContextProvider } from "./context/registerContext";
 export default function Home() {
   return (
     <>
-      <Header />
+      <Header>
+        <Image
+          role="logo"
+          priority
+          src={logo}
+          width={75}
+          height={75}
+          alt="logo"
+          blurDataURL={"@/../public/icons/budget.png"}
+        />
+      </Header>
       <RegisterContextProvider>
         <Main />
       </RegisterContextProvider>
