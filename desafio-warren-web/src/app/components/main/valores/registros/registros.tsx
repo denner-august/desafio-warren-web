aaa23e1a6c891d148297f5b0d157dadcc44f4a5f
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-28 18:10:37 -0300

    Update registros.tsx

--- desafio-warren-web/src/app/components/main/valores/registros/registros.tsx
+++ desafio-warren-web/src/app/components/main/valores/registros/registros.tsx
@@ -4,9 +4,11 @@ export function RenderRegistros({
   RegisterSearch,
   controller,
 }: RenderRegistrosProps) {
-  return RegisterSearch?.map(({ id, title, description, amount, status }) => {
+  return RegisterSearch?.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  ).map(({ id, title, description, amount, status }) => {
     return (
-      <tr key={id} onClick={() => controller(id)}>
+      <tr key={id} onClick={() => controller(id)} role="transactions">
         <td>{title}</td>
         <td>{description}</td>
         <td>{status}</td>
